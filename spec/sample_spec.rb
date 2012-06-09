c8d5372fecb624d776e2e2b5df831e1b43a13d68
zakuni <kunio038@gmail.com>
2012-06-09 17:20:35 +0900

    added sample of rspec

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -1,3 +1,12 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
+require 'sample'
 
+describe Sample do
+  before do
+    @sample = Sample.new
+  end
+  it 'should say hello' do
+    @sample.sayhello.should eq("hello")
+  end
+end
